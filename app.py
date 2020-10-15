a520ecc96538ec3cdcd33dd4753925a9cd0a3dbc
unknown <vishnurajrajendran17@gmail.com>
2020-10-15 16:33:53 +0530

    Updated commits

--- app.py
+++ app.py
@@ -15,59 +15,20 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-df = pd.read_excel("sample_data.xlsx")
+file_name = "sample_data.csv"
+
+if "csv" in file_name:
+    df = pd.read_csv(file_name)
+elif "xls" in file_name:
+    df = pd.read_excel(file_name)
 
 df.reset_index(inplace=True)
-print(df[:5])
+#print(df[:5])
 
 df_columns = df.columns
-# def parse_contents(contents, filename, date):
-#     content_type, content_string = contents.split(',')
-#
-#     decoded = base64.b64decode(content_string)
-#     try:
-#         if 'csv' in filename:
-#             # Assume that the user uploaded a CSV file
-#             df = pd.read_csv(
-#                 io.StringIO(decoded.decode('utf-8')))
-#         elif 'xls' in filename:
-#             # Assume that the user uploaded an excel file
-#             df = pd.read_excel(io.BytesIO(decoded))
-#         df_columns = df.columns
-#     except Exception as e:
-#         print(e)
-#         return html.Div([
-#             'There was an error processing this file.'
-#         ])
-#
-#     return html.Div([
-#         html.H5(filename),
-#         html.H6(datetime.datetime.fromtimestamp(date))
-#     ])
+
 app.layout = html.Div([
     html.H1('Analytics Dashboard',style={'text-align':'center'}),
-    # html.Div([
-    # dcc.Upload(
-    #     id='upload-data',
-    #     children=html.Div([
-    #         'Drag and Drop or ',
-    #         html.A('Select Files')
-    #     ]),
-    #     style={
-    #         'width': '90%',
-    #         'height': '60px',
-    #         'lineHeight': '60px',
-    #         'borderWidth': '1px',
-    #         'borderStyle': 'dashed',
-    #         'borderRadius': '5px',
-    #         'textAlign': 'center',
-    #         'margin': '10px'
-    #     },
-    #     # Allow multiple files to be uploaded
-    #     multiple=False
-    # ),
-    # html.Div(id='output-data-upload'),
-    # ]),
     html.Div([
     html.H5('Xaxis', className="four columns"),
     dcc.Dropdown(id="column-select1",
@@ -79,6 +40,7 @@ app.layout = html.Div([
                 ),
     html.Br(),
     html.H6('Top Value Filter',style={'textAlign': 'left'}, className="four columns"),
+    html.Label('*- 0 value shows all the values'),
     dcc.Input(
             id="top", type="number",
             debounce=True, placeholder="Debounce True",
@@ -102,19 +64,6 @@ app.layout = html.Div([
     dcc.Graph(id="my_first_dash_graph",figure={})
     ],className='twelve columns',)
 ])
-
-
-
-# @app.callback(Output('output-data-upload', 'children'),
-#               [Input('upload-data', 'contents')],
-#               [State('upload-data', 'filename'),
-#                State('upload-data', 'last_modified')])
-# def update_output(list_of_contents, list_of_names, list_of_dates):
-#     if list_of_contents is not None:
-#         children = [
-#             parse_contents(c, n, d) for c, n, d in
-#             zip(list_of_contents, list_of_names, list_of_dates)]
-#         return children
 
 @app.callback(
     Output('my_first_dash_graph', 'figure'),
