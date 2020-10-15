d0ef8b9c5f5de02f6f1489e7d16360f5a01dca3e
Vishnuraj Rajendran <40426387+i-vishnuraj@users.noreply.github.com>
2020-10-15 18:39:06 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import pandas as pd
+import numpy as np
 import plotly.express as px
 
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import base64
-import datetime
+import datetime as dt
 import io
 from dash.dependencies import Input, Output, State
 import dash_table
@@ -15,17 +16,31 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-df = pd.read_csv("sample_data.csv")
+file_name = "sample_data.xlsx"
+
+if "csv" in file_name:
+    df = pd.read_csv(file_name)
+elif "xls" in file_name:
+    df = pd.read_excel(file_name)
 
 df.reset_index(inplace=True)
-print(df[:5])
+#print(df[:5])
+df_datefields = [i for i in df.columns if np.issubdtype(df[i].dtype, np.datetime64)]
+
+if len(df_datefields):
+    for i in df_datefields:
+        new_colmonth_name = i + "_Month"
+        #print(new_colmonth_name)
+        df[new_colmonth_name] = df[i].dt.strftime('%b %y')
+        new_col_hour_name = i + "_Hour"
+        df[new_col_hour_name] = df[i].dt.strftime('%H')
 
 df_columns = df.columns
 
 app.layout = html.Div([
-    html.H1('Analytics Dashboard',style={'text-align':'center'}),
+    html.H2('Analytics Dashboard',style={'text-align':'center'}),
     html.Div([
-    html.H5('Xaxis', className="four columns"),
+    html.H6('X-axis', className="four columns"),
     dcc.Dropdown(id="column-select1",
                 options=[
                     {'label':i,'value':i} for i in df_columns if i != "index"],
@@ -34,20 +49,37 @@ app.layout = html.Div([
                 style={'width':"60%"}
                 ),
     html.Br(),
-    html.H6('Top Value Filter',style={'textAlign': 'left'}, className="four columns"),
-    html.Label('*- 0 value shows all the values'),
+    html.Label('Top X Value Filter (*- 0 value shows all)',style={'textAlign': 'left'}, className="four columns"),
     dcc.Input(
-            id="top", type="number",
+            id="topx", type="number",
+            debounce=True, placeholder="Debounce True",
+            value=5
+        ),
+    html.Br(),
+    html.Br(),
+    html.H6('Y-axis', className="four columns"),
+    dcc.Dropdown(id="column-select2",
+                options=[
+                    {'label':i,'value':i} for i in df_columns if i != "index" and type(i) != "object"],
+                multi=False,
+                value=df_columns[0],
+                style={'width':"60%"}
+                ),
+    html.Br(),
+    html.Label('Top Y Value Filter (*- 0 value shows all)',style={'textAlign': 'left'}, className="four columns"),
+    dcc.Input(
+            id="topy", type="number",
             debounce=True, placeholder="Debounce True",
             value=5
         ),
     ],className='four columns',),
     html.Div([
-    html.H5('Type of Graph', className="four columns"),
-    dcc.Dropdown(id="column-select2",
+    html.H6('Type of Graph', className="four columns"),
+    dcc.Dropdown(id="graph-select",
                 options=[
                     {'label':"Bar",'value':"bar"},
-                    {'label':"Pie",'value':"pie"}],
+                    {'label':"Pie",'value':"pie"},
+                    {'label':"Heat Map",'value':"heatmap"}],
                 multi=False,
                 value="bar",
                 style={'width':"60%"}
@@ -60,43 +92,42 @@ app.layout = html.Div([
     ],className='twelve columns',)
 ])
 
-
-
 @app.callback(
     Output('my_first_dash_graph', 'figure'),
-    [Input('column-select1', 'value'),
-    Input('column-select2', 'value'),
-    Input('top', 'value')])
-def update_figure(col,graph_type,top_val):
-    if "Repetitive" in col:
-        value_counts = df[df["Repetitive/Nonrepetitive"] == "Repetitive"][col].value_counts(dropna=True, sort=True)
+    [Input('column-select1', 'value'), Input('column-select2', 'value'),
+    Input('graph-select', 'value'), Input('topx', 'value'), Input('topy', 'value')])
+def update_figure(col1,col2,graph_type,topx_val,topy_val):
+    if "Repetitive" in col1:
+        if col2 == "":
+            value_counts = df[df["Repetitive/Nonrepetitive"] == "Repetitive"][col1].value_counts(dropna=True, sort=True)
+        else:
+            pass
     else:
-        value_counts = df[col].value_counts(dropna=True, sort=True)
+        value_counts = df[col1].value_counts(dropna=True, sort=True)
     # solution here
     df_val_counts = pd.DataFrame(value_counts)
     df_value_counts = df_val_counts.reset_index()
     df_value_counts.columns = ['Category', 'count_cat']
     if graph_type == "bar":
-        if top_val > 0:
-            fig = px.bar(df_value_counts[:top_val], x="Category", y="count_cat",text="count_cat",color="Category")
+        if topx_val > 0:
+            fig = px.bar(df_value_counts[:topx_val], x="Category", y="count_cat",text="count_cat",color="Category")
         else:
             fig = px.bar(df_value_counts, x="Category", y="count_cat",text="count_cat",color="Category")
         fig.update_layout(transition_duration=500,
-        title=str(col)+" Distribution",
-        xaxis_title=col,
+        title=str(col1)+" Distribution",
+        xaxis_title=col1,
         yaxis_title="Count",
         showlegend=False)
     elif graph_type == "pie":
-        if top_val > 0:
-            fig = px.pie(df_value_counts[:top_val], values="count_cat", names="Category",hole=0.4)
+        if topx_val > 0:
+            fig = px.pie(df_value_counts[:topx_val], values="count_cat", names="Category",hole=0.4)
         else:
             fig = px.pie(df_value_counts, values="count_cat", names="Category",hole=0.4)
         fig.update_traces(textposition='outside', textinfo='label+text+percent')
         fig.update_layout(transition_duration=500,
-        title=str(col)+" Distribution",
+        title=str(col1)+" Distribution",
         showlegend=False)
-
     return fig
 
 if __name__=='__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True,port="8010",host="127.0.0.1")
